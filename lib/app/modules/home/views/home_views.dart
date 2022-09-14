ca288ca8eb3d2da57c9430d55023ed68a13fbc41
marcelacrvalho <marcelacrvalho@icloud.com>
2022-09-14 19:57:05 -0300

    Altera design

--- lib/app/modules/home/views/home_views.dart
+++ lib/app/modules/home/views/home_views.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
+import 'package:termooo/app/modules/home/controllers/home_controllers.dart';
 import 'package:termooo/app/modules/home/widgets/text_field_widgets.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: HomeController.to.checkWord,
         child: const Icon(Icons.send),
       ),
       body: _body(),
@@ -19,56 +20,70 @@ class HomeView extends StatelessWidget {
   Widget _body() {
     return SafeArea(
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                  'Adivinhe qual é a palavra',
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Adivinhe qual é a palavra',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20.0,
                 ),
               ),
-            ),
-            _populateInput(),
-            _populateInput(),
-            _populateInput(),
-            _populateInput(),
 
-          ],
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextField(
+                      color: HomeController.to.isGreen.value
+                          ? Colors.green
+                          : Colors.black26,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextField(
+                      color: HomeController.to.isGreen.value
+                          ? Colors.green
+                          : Colors.black26,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextField(
+                      color: HomeController.to.isGreen.value
+                          ? Colors.green
+                          : Colors.black26,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextField(
+                      color: HomeController.to.isGreen.value
+                          ? Colors.green
+                          : Colors.black26,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextField(
+                      color: HomeController.to.isGreen.value
+                          ? Colors.green
+                          : Colors.black26,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _populateInput() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomTextField(Colors.black26),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomTextField(Colors.black26),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomTextField(Colors.black26),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomTextField(Colors.black26),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomTextField(Colors.black26),
-        ),
-      ],
-    );
-  }
 }
