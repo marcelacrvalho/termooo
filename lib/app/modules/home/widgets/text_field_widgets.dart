afd6a83b5945f919ac1e961126160ecdaa7c07c4
marcelacrvalho <marcelacrvalho@icloud.com>
2022-09-15 15:08:51 -0300

    Altera design

--- lib/app/modules/home/widgets/text_field_widgets.dart
+++ lib/app/modules/home/widgets/text_field_widgets.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:get/route_manager.dart';
-import 'package:termooo/app/modules/home/controllers/home_controllers.dart';
 
 class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
   final Color color;
 
-  const CustomTextField({Key? key, required this.color}) : super(key: key);
+  const CustomTextField({Key? key, required this.controller, required this.color}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200,
       width: 100,
-      child:TextField(
+      child: TextField(
+        controller: controller,
         textAlign: TextAlign.center,
         maxLengthEnforcement: MaxLengthEnforcement.enforced,
         maxLength: 1,
